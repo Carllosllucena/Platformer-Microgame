6a6947e44382ccf09139962eaebd8d2d526b380e
Carllosllucena <novac992107020@gmail.com>
2025-06-02 14:25:38 -0300

    Commit inicial do preojeto unity

---
+++ Assets/Mod Assets/Mod Resources/Scripts/Creditos.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Creditos : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Jogo desenvolvido por Carlos Ervécio Lucena Cavalcante - 2025");
+    }
+}
